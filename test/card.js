03cc17d0e5cbafe65f3b02e1a2f7f1e492169b9e
Sam Collard <swift@swifteagle.co.uk>
2016-08-11 23:04:17 +0100

    Handle gold cards

--- test/card.js
+++ test/card.js
@@ -11,6 +11,11 @@ describe('Translate', () => {
       var translatedCard = translate.card(testData.card.json)
       expect(translatedCard).to.be.card_like(testData.card.xml)
     })
+
+    it('should translate a gold card correctly', () => {
+      var translatedCard = translate.card(testData.goldCard.json)
+      expect(translatedCard).to.be.card_like(testData.goldCard.xml)
+    })
   })
 })
 
@@ -47,20 +52,73 @@ function getTestdata() {
         ]
       },
       xml: `
-      <card name="Kjeldoran Home Guard" id="a8434484-69af-b76a-7a2c-83837e807222">
-        <property name="Cost" value="{3}{W}" />
-        <property name="CMC" value="4" />
-        <property name="Color" value="White" />
-        <property name="Type" value="Creature" />
-        <property name="Subtype" value="Human Soldier" />
-        <property name="Rarity" value="Uncommon" />
-        <property name="Rules" value="At end of combat, if Kjeldoran Home Guard attacked or blocked this combat, put a -0/-1 counter on Kjeldoran Home Guard and put a 0/1 white Deserter creature token onto the battlefield." />
-        <property name="Power" value="1" />
-        <property name="Toughness" value="6" />
-        <property name="PT Box" value="1 / 6" />
-        <property name="Artist" value="Andi Rusu" />
-        <property name="MultiverseId" value="3200" />
-      </card>
+        <card name="Kjeldoran Home Guard" id="a8434484-69af-b76a-7a2c-83837e807222">
+          <property name="Cost" value="{3}{W}" />
+          <property name="CMC" value="4" />
+          <property name="Color" value="White" />
+          <property name="Type" value="Creature" />
+          <property name="Subtype" value="Human Soldier" />
+          <property name="Rarity" value="Uncommon" />
+          <property name="Rules" value="At end of combat, if Kjeldoran Home Guard attacked or blocked this combat, put a -0/-1 counter on Kjeldoran Home Guard and put a 0/1 white Deserter creature token onto the battlefield." />
+          <property name="Power" value="1" />
+          <property name="Toughness" value="6" />
+          <property name="PT Box" value="1 / 6" />
+          <property name="Artist" value="Andi Rusu" />
+          <property name="MultiverseId" value="3200" />
+        </card>
+      `
+    },
+    goldCard: {
+      json: {
+        "artist": "Cyril Van Der Haegen",
+        "cmc": 2,
+        "colorIdentity": [
+          "U",
+          "B"
+        ],
+        "colors": [
+          "Blue",
+          "Black"
+        ],
+        "flavor": "The scullers beneath Esper keep strixes as trained pets, and set them loose when a fare refuses to pay.",
+        "id": "7d1db2828fcca5c63e7761970801b017e4eee22e",
+        "imageName": "tidehollow strix",
+        "layout": "normal",
+        "manaCost": "{U}{B}",
+        "mciNumber": "203",
+        "multiverseid": 175015,
+        "name": "Tidehollow Strix",
+        "number": "203",
+        "power": "2",
+        "rarity": "Common",
+        "subtypes": [
+          "Bird"
+        ],
+        "text": "Flying\nDeathtouch (Any amount of damage this deals to a creature is enough to destroy it.)",
+        "toughness": "1",
+        "type": "Artifact Creature \u2014 Bird",
+        "types": [
+          "Artifact",
+          "Creature"
+        ]
+      },
+      xml: `
+        <card name="Tidehollow Strix" id="f3984c7c-b64c-20a5-1700-77b3350cc455">
+          <property name="Cost" value="{U}{B}" />
+          <property name="CMC" value="2" />
+          <property name="Color" value="Multicolor Blue Black" />
+          <property name="Type" value="Artifact Creature" />
+          <property name="Subtype" value="Bird" />
+          <property name="Rarity" value="Common" />
+          <property name="Rules" value="Flying&#xD;&#xA;Deathtouch (Any amount of damage this deals to a creature is enough to destroy it.)" />
+          <property name="Power" value="2" />
+          <property name="Toughness" value="1" />
+          <property name="PT Box" value="2 / 1" />
+          <property name="Artist" value="Cyril Van Der Haegen" />
+          <property name="Number" value="203" />
+          <property name="Flavor" value="The scullers beneath Esper keep strixes as trained pets, and set them loose when a fare refuses to pay." />
+          <property name="MultiverseId" value="175015" />
+        </card>
       `
     }
   }
--- translate.js
+++ translate.js
@@ -1,28 +1,38 @@
 var xml = require('xml')
 var UUID = require('node-uuid')
 
-var xmlProperty = (name, value) => ({
+var xmlProperty = (name, value) => (name && value && {
   property: {
     _attr: { name, value }
   }
 })
 
 function translateCard(card) {
+  var handle = {
+    colors: colors => (colors.length > 1 ? 'Multicolor ' : '') + colors.join(' '),
+    text: text => text.replace('\n', '\r\n'),
+    join: field => field.join(' ')
+  }
+
   var rv = xml({
     card: [
       { _attr: { name: card.name, id: UUID.v4() + 'aaaa' } },
       xmlProperty('Cost',         card.manaCost),
       xmlProperty('CMC',          card.cmc),
-      xmlProperty('Color',        card.colors[0]),
-      xmlProperty('Type',         card.types.join(' ')),
-      xmlProperty('Subtype',      card.subtypes.join(' ')),
       xmlProperty('Rarity',       card.rarity),
-      xmlProperty('Rules',        card.text),
       xmlProperty('Power',        card.power),
       xmlProperty('Toughness',    card.toughness),
-      xmlProperty('PT Box',       `${card.power} / ${card.toughness}`),
       xmlProperty('Artist',       card.artist),
-      xmlProperty('MultiverseId', card.multiverseid)
+      xmlProperty('MultiverseId', card.multiverseid),
+      xmlProperty('Number',       card.mciNumber),
+      xmlProperty('Flavor',       card.flavor),
+
+      xmlProperty('Type',         handle.join(card.types)),
+      xmlProperty('Subtype',      handle.join(card.subtypes)),
+      xmlProperty('Color',        handle.colors(card.colors)),
+      xmlProperty('Rules',        handle.text(card.text)),
+
+      xmlProperty('PT Box',       `${card.power} / ${card.toughness}`)
     ]
   })
   return rv
