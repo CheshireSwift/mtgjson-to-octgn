d954a9e0222cf290b5a485317382ef01c27edc64
Sam Collard <swift@swifteagle.co.uk>
2016-08-13 21:07:40 +0100

    Expose card translation to command line invocation

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 #!/usr/bin/node
 
+var _ = require('lodash')
 var program = require('commander')
 
-var xml = require('xml')
+var translate = require('./translate')
 
+var method
 program
-  .version('0.0.0')
+  .version('1.0.0')
+  .arguments('<method>')
+  .action(m => method = m)
   .parse(process.argv)
 
-console.log('hi')
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+process.stdin.on('data', _.flow(
+  JSON.parse,
+  translate[method],
+  x => process.stdout.write(x)
+))
 
